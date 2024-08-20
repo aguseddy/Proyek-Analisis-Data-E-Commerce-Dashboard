d0cec98d36cddccb2b51fd58a32e28246d6dbc28
Eddy Muntina Dharma <85097996+aguseddy@users.noreply.github.com>
2024-08-20 13:57:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Tampilkan judul
 st.title('Proyek Analisa Data : E-Commerce')
@@ -30,5 +31,37 @@ st.bar_chart(
     horizontal=True
 )
 
+# Visualisasi RQ2
+st.subheader("Perkembangan omset penjualan per Bulan")
+
+st.line_chart(
+    rq2_df,
+    x='thn_bln',
+    y='jual',
+    x_label = 'Periode',
+    y_label = 'Omset',
+)
+
+
+# Visualisasi RQ3
+st.subheader("Segmentasi Customer")
+
+fig, ax = plt.subplots(figsize=(20, 10))
+
+sns.barplot(
+    x="jml_customer", 
+    y="customer_segment",
+    data=rq3_df.sort_values(by="customer_segment", ascending=False),
+    hue="customer_segment"
+)
+
+ax.set_title("Segmentasi Customer", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.xaxis.grid(True)
+ax.yaxis.grid(True)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
 
 st.caption('Copyright (c) 2024')
